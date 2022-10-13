abd89eb38779cb2aa74279ab49613d968483491a
houda <hh1845120@gmail.com>
2022-10-13 16:00:31 +0100

    add files

---
+++ index.js
@@ -0,0 +1,8 @@
+import Etudiant,{Etablissement,info} from './Etudiant.js'
+console.log(Etablissement)
+let etd1= new Etudiant("Rami",33)
+console.log(info(etd1))
+
+const salutation="salut"
+const nom ="Rami"
+console.log(salutation +" "+ nom)
